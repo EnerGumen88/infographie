4ab7207281d003ce98f80ec3954f891eda7c552a
EnerGumen <fredolecoco@hotmail.fr>
2015-02-18 18:45:11 +0100

    Add texture vec to model

--- model.cpp
+++ model.cpp
@@ -6,7 +6,7 @@
 #include "model.h"
 
 
-Model::Model (const char *filename) : verts_(), norms_(), faces_(){
+Model::Model (const char *filename) : verts_(), norms_(), textures_(), faces_(){
   std::ifstream file(filename, std::ios::in);
   
   if (file){
@@ -21,6 +21,12 @@ Model::Model (const char *filename) : verts_(), norms_(), faces_(){
 	for (int i=0;i<3;i++) line_stream >> v[i];
 	verts_.push_back(v);
       }
+      else if (line.compare(0, 2, "vt") == 0){
+	line_stream >> trash_char >> trash_char;
+	Vec2f vt;
+	for (int i=0;i<2;i++) line_stream >> vt[i];
+	textures_.push_back(vt);
+      }
       else if (line.compare(0, 2, "vn") == 0){
 	line_stream >> trash_char >> trash_char;
 	Vec3f vn;
@@ -28,12 +34,14 @@ Model::Model (const char *filename) : verts_(), norms_(), faces_(){
 	norms_.push_back(vn);
       }
       else if (line.compare(0, 1, "f") == 0){
-	std::vector<int> f;
+	std::vector<Vec3i> f;
 	line_stream >> trash_char;
-	int trash_int,fi;
+	Vec3i fi;
 	for (int i=0;i<3;i++) {
-	  line_stream >> fi >> trash_char >> trash_int >> trash_char >> trash_int;
-	  fi--;
+	  line_stream >> fi[0] >> trash_char >> fi[1] >> trash_char >> fi[2];
+	  fi[0]--;
+	  fi[1]--;
+	  fi[2]--;
 	  f.push_back(fi);
 	}
 	faces_.push_back(f);
@@ -56,7 +64,7 @@ int Model::nfaces() {
 }
 
 
-std::vector<int> Model::face(int idx) {
+std::vector<Vec3i> Model::face(int idx) {
  return faces_[idx];
 }
 
@@ -66,4 +74,8 @@ Vec3f Model::vert(int i) {
 
 Vec3f Model::norm(int i) {
  return norms_[i];
+}
+
+Vec2f Model::texture(int i) {
+ return textures_[i];
 }
--- model.h
+++ model.h
@@ -8,14 +8,16 @@ class Model {
 private:
  std::vector<Vec3f> verts_;
  std::vector<Vec3f> norms_;
- std::vector<std::vector<int> > faces_;
+ std::vector<Vec2f> textures_;
+ std::vector<std::vector<Vec3i> > faces_;
 public:
  Model(const char *filename);
  int nverts();
  int nfaces();
  Vec3f vert(int i);
-	Vec3f norm(int i);
- std::vector<int> face(int idx);
+ Vec3f norm(int i);
+ Vec2f texture(int i);
+ std::vector<Vec3i> face(int idx);
 };
 
 #endif
